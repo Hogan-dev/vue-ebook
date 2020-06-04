6b26714110d278e4ebe19ba48e0cd6fd6429dc76
Hogan <hogan@dcpt.online>
2020-06-04 22:11:37 +0800

    字体UI设置

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -1,9 +1,72 @@
 <template>
-
+  <div class="ebook-reader">
+    <div id="read"></div>
+  </div>
 </template>
 
 <script>
-  export default {}
+import { ebookMixin } from '../../utils/mixin'
+import Epub from 'epubjs'
+global.ePub = Epub
+export default {
+  mixins: [ebookMixin],
+  methods: {
+    prevPage () {
+      if (this.rendition) {
+        this.rendition.prev()
+        this.hideTitleAndMenu()
+      }
+    },
+    nextPage () {
+      if (this.rendition) {
+        this.rendition.next()
+        this.hideTitleAndMenu()
+      }
+    },
+    toggleTitleAndMenu () {
+      // this.$store.dispatch('setMenuVisible', !this.menuVisible)
+      this.setMenuVisible(!this.menuVisible)
+    },
+    hideTitleAndMenu () {
+      // this.$store.dispatch('setMenuVisible', false)
+      this.setMenuVisible(false)
+    },
+    initEpub () {
+      const url = 'http://192.168.0.223:8081/epub/' + this.fileName + '.epub'
+      this.book = new Epub(url)
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        method: 'default'
+      })
+      this.rendition.display()
+      this.rendition.on('touchstart', event => {
+        this.touchStartX = event.changedTouches[0].clientX
+        this.touchStartTime = event.timeStamp
+      })
+      this.rendition.on('touchend', event => {
+        const offsetX = event.changedTouches[0].clientX - this.touchStartX
+        const time = event.timeStamp - this.touchStartTime
+
+        if (time < 500 && offsetX > 40) {
+          this.prevPage()
+        } else if (time < 500 && offsetX < -40) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+        }
+        event.preventDefault()
+        event.stopPropagation()
+      })
+    }
+  },
+  mounted () {
+    const fileName = this.$route.params.fileName.split('|').join('/')
+    this.setFileName(fileName).then(() => {
+      this.initEpub()
+    })
+  }
+}
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
--- src/views/ebook/index.vue
+++ src/views/ebook/index.vue
@@ -1,9 +1,22 @@
 <template>
-
+  <div class="ebook">
+    <ebook-title></ebook-title>
+    <ebook-reader></ebook-reader>
+    <ebook-menu></ebook-menu>
+  </div>
 </template>
 
 <script>
-  export default {}
+import EbookReader from '../../components/ebook/EbookReader'
+import EbookTitle from '../../components/ebook/EbookTitle'
+import EbookMenu from '../../components/ebook/EbookMenu'
+export default {
+  components: {
+    EbookReader,
+    EbookTitle,
+    EbookMenu
+  }
+}
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
